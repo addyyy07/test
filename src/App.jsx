07d5efc92a18e378eed12726a2dc211158f50f92
addyyy07 <ruvaifabhagvani786@gmail.com>
2024-01-27 16:08:10 +0530

    update

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Test from './compo/Test'
+
+function App() {
+
+  return (
+    <>
+      <Test surname="Bhagvani" />
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/compo/Test.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Test({surname,sem}){
+    return(
+        <div>
+            <h1>Ruvaifa</h1>
+            <h1> {surname}</h1>
+            <h1> {sem}</h1>
+        </div>
+    )
+}
+
+export default Test
